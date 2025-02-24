f0ff64e9b9209a2ba9e880e0c3b54449364951db
Meiman Ryland Hulu <94421861+meimanhulu@users.noreply.github.com>
2025-02-25 02:57:14 +0700

    push Positif TC

---
+++ Keywords/pages/MenuPages.groovy
@@ -0,0 +1,39 @@
+package pages
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import global.ObjectGlobalPages
+import internal.GlobalVariable
+
+public class MenuPages {
+	ObjectGlobalPages ObjectGlobal = new ObjectGlobalPages()
+
+	private TestObject menuField
+	private TestObject subMenuField
+
+	def  MenuPages(String menu, String subMenu) {
+		menuField = ObjectGlobal.getTestObject("//p[text()='${menu}']")
+		subMenuField = ObjectGlobal.getTestObject("//p[text()='${subMenu}']")
+	}
+
+	def menu() {
+		WebUI.click(menuField)
+		WebUI.click(subMenuField)
+	}
+}
---
+++ Test Suites/TSHalamanLaporan/Cuti/TS03FCuti.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS03FCuti</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3bae8217-6eb1-4f45-bedb-e9fca4f3f7ed</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b7645fd-3481-48df-aef9-aa573c9383ed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2HalamanLaporan/TC02Kehadiran/Positif/login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc11177e-a322-4230-95c5-b4a21ddad1be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2HalamanLaporan/TC03Cuti/Positif/Cuti</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>82f6d23a-f53e-49e9-8141-7ce49c8bb12b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2HalamanLaporan/TC03Cuti/Positif/logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
