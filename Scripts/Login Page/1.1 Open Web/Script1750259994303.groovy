c916549139ac9a4c207dbd2beab2119c5275817c
Meiman Ryland Hulu <94421861+meimanhulu@users.noreply.github.com>
2025-06-20 09:33:40 +0700

    push

--- Scripts/Login Page/1.1 Open Web/Script1750259994303.groovy
+++ Scripts/Login Page/1.1 Open Web/Script1750259994303.groovy
@@ -3,9 +3,6 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-
-import javax.wsdl.Import
-
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
@@ -17,19 +14,6 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable
-import pages.KehadiranPages
-import pages.MenuPages
-import pages.SubMenuPages
-
+import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-
-MenuPages menuPage = new MenuPages("Laporan", "Kehadiran")
-SubMenuPages submenuPage = new SubMenuPages()
-
-KehadiranPages kehadiranPage = new KehadiranPages("January", "2021", "1", "February", "2025", "23")
-
-
-menuPage.menu()
-kehadiranPage.kehadiran("ADITYA RIDWAN NUGRAHA", "Iforte")
 
